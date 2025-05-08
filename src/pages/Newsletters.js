12be85982269897e9b81c4a316e958c223b865a4
Kaleb White <kalebw02@ad.unc.edu>
2025-05-07 20:38:03 -0400

    Add functionality to wait for the iframe to load and display something separate while it does load

--- src/pages/Newsletters.js
+++ src/pages/Newsletters.js
@@ -1,19 +1,38 @@
+import { useState } from "react";
+
+const NewsletterWait = ({loaded}) => {
+	if (!loaded) {
+		return (
+			<h1>hello</h1>
+		);
+	} else {
+		return (<div style={{display: 'none'}}></div>);
+	}
+};
+
 const Newsletters = () => {
+	const [loaded, setLoaded] = useState(false);
+
     return (
       <div className="flex justify-center w-full">
         <div className="
-          mt-2 mb-2 ml-1 mr-1
-          p-2
-          rounded-lg border border-black
-          flex justify-center
-          "
+          	mt-2 mb-2 ml-1 mr-1
+          	p-2
+          	rounded-lg border border-black
+          	flex justify-center
+        "
         >
-          <iframe
-            src="https://us17.campaign-archive.com/home/?u=73a81bd71c0831f152215407f&id=f8de748d7d"
-            title="newsletters"
-            height="800px"
-            width="550px"
-          />
+			<iframe
+				src="https://us17.campaign-archive.com/home/?u=73a81bd71c0831f152215407f&id=f8de748d7d"
+				title="newsletters"
+				height="800px"
+				width="550px"
+				onLoad={() => setLoaded(true)}
+				style={{
+					display: loaded ? 'block' : 'none'
+				}}
+			/>
+			<NewsletterWait loaded={loaded}/>
         </div>
       </div>
     );
