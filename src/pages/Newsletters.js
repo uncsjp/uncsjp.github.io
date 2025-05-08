6bc3916bc7ad7ef85c1ee3be91d4e202f4869a4e
Kaleb White <kalebw02@ad.unc.edu>
2025-05-07 20:04:09 -0400

    Add mailchimp newsletter via iframe

--- src/pages/Newsletters.js
+++ src/pages/Newsletters.js
@@ -1,6 +1,20 @@
 const Newsletters = () => {
     return (
-      <div>
+      <div className="flex justify-center w-full">
+        <div className="
+          mt-2 mb-2 ml-1 mr-1
+          p-2
+          rounded-lg border border-black
+          flex justify-center
+          "
+        >
+          <iframe
+            src="https://us17.campaign-archive.com/home/?u=73a81bd71c0831f152215407f&id=f8de748d7d"
+            title="newsletters"
+            height="800px"
+            width="550px"
+          />
+        </div>
       </div>
     );
 };
