ca0171b3abbf45ce7cc5ae3bba62ecf0af0d0c64
Kaleb White <kalebw02@ad.unc.edu>
2025-05-16 13:31:30 -0400

    Finish non-dynamic principles and goals

--- src/pages/PrinciplesAndGoals.js
+++ src/pages/PrinciplesAndGoals.js
@@ -7,7 +7,7 @@ const PrinciplesAndGoals = () => {
         <TextBox text={"Loading..."} header={"Loading..."} className="animate-pulse" />
     );
 
-    // Refactor to custom hook
+    // TODO: Refactor to custom hook
     useEffect(() => {
         fetch('text/text.json')
             .then(response => response.json()) // Convert body to json
@@ -15,33 +15,35 @@ const PrinciplesAndGoals = () => {
             .then(section => { // Reformat json as textboxes
                 let o = []
                 for (let tb in section) {
-                    o.push(<div key={section[tb].id}><TextBox header={section[tb].header} text={section[tb].text} /></div>)
+                    o.push(<TextBox key={section[tb].id} header={section[tb].header} text={section[tb].text} />)
                 }
                 return o
-            })
-            .then(section => { // Add in images
-                return [
-                section.slice(0,1),
-                <Image key={10} url={"protest_from_south.jpg"}/>, // Arbitrary keys cause no rerenders
-                section.slice(1,3),
-                <Image key={20} url={"peoples_grad.jpg"} />,
-                section.slice(3)
-                ]
             })
             .then(jsx => { // Set content
                 set_text(jsx);
             })
             .catch((e => console.error(e)));
         return () => {}
-    }, [set_text])
+    }, [])
 
     return (
         <div className="
-            grid grid-cols-1 md:grid-cols-2
+            flex flex-col
             w-full
             justify-center
         ">
-           {text_boxes}
+            {/* TODO: refactor to be dynamic */}
+            <div className="contents md:flex md:flex-col">
+                {text_boxes[0]}
+                <Image key={10} url={"protest_from_south.jpg"}/>
+                {text_boxes[1]}
+            </div>
+            <div className="contents md:flex md:flex-col">
+                {text_boxes[2]}
+                <Image key={20} url={"peoples_grad.jpg"} />
+                {text_boxes[3]}
+                {text_boxes[4]}
+            </div>
         </div>
     );
 };
