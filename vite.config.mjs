94e8405a84c04ce7097f1b6025c3bfad883c2f5c
Kaleb White <kalebw02@ad.unc.edu>
2025-07-07 15:13:36 -0400

    Change vite base route

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react(), tailwindcss()]
+    base: '/site/',
+    plugins: [react(), tailwindcss()],
 })
